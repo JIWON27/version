862d58e30df0c050cc76eb249b6676075cdbdbad
jiwon <mjo4260@naver.com>
2023-07-12 10:52:59 +0900

    Edit main.yml

--- src/main/java/com/example/version/controller/TestController.java
+++ src/main/java/com/example/version/controller/TestController.java
@@ -1,13 +1,27 @@
 package com.example.version.controller;
 
+import com.example.version.reopsitory.VersionResitory;
+import com.example.version.web.dto.VersionResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class TestController {
-
+  private final VersionResitory versionResitory;
   @RequestMapping("/api/test")
   public String test(){
+
     return "Hello World";
+  }
+
+  @GetMapping("api/latest/versions")
+  public ResponseEntity<VersionResponseDto> findArticle(@PathVariable Long id){
+    return  ResponseEntity.ok()
+        .body(new VersionResponseDto(versionResitory.findById(id)));
   }
 }
